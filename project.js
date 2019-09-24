94cf4f09632dabbea407215a250da2447c049c78
Derek Liu <liuderek97@gmail.com>
2019-09-24 14:38:08 +1000

    remake project

--- project.js
+++ project.js
@@ -2,17 +2,4 @@
 
 const fetch = require('node-fetch');
 
-console.log("Welcome to the Karoake machine, Select one of the genres by press 1, 2 or 3")
 
-let genres = []
-
-const getGenres = async () => {  
-    let genre1 = await fetch('https://api.musixmatch.com/ws/1.1/');
-    let genre1Data = await genre1.json();
-    genres.push(genre1.value)
-    return genre1;
-}
-
-getGenres()
-    .then(genre1 => console.log(genre1))
-    .catch(err => console.log(err));
