9770b64540315c89f5f37f0f1592826f3a73ef59
Derek Liu <liuderek97@gmail.com>
2019-09-24 12:25:00 +1000

    new master`

--- project.js
+++ project.js
@@ -2,4 +2,10 @@
 
 const fetch = require('node-fetch');
 
-var genres
+console.log('Welcome to the Karaoke machine')
+
+let genres = []
+let url = 'https://api.musixmatch.com/ws/1.1/'
+
+fetch(url)
+  .then(respone => Response.json)
