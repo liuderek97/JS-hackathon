41306241fb5132d0cc35e1659094c14bfc32e897
hussainmusawi <49133927+hussainmusawi@users.noreply.github.com>
2019-09-24 11:50:30 +1000

    first commit

--- project.js
+++ project.js
@@ -2,5 +2,17 @@
 
 const fetch = require('node-fetch');
 
-var genres
-console.log("Whats up")
+console.log("Welcome to the Karoake machine, Select one of the genres by press 1, 2 or 3")
+
+let genres = []
+
+const getGenres = async () => {  
+    let genre1 = await fetch('https://api.musixmatch.com/ws/1.1/');
+    let genre1Data = await genre1.json();
+    genres.push(genre1.value)
+    return genre1;
+}
+
+getGenres()
+    .then(genre1 => console.log(genre1))
+    .catch(err => console.log(err));
